5c82e09f0f2a5a298e91f9df4727bf6a4d200bc4
THEGAMEDEV0 <90412798+THEGAMEDEV0@users.noreply.github.com>
2022-11-25 18:26:57 +0000

    Added more access codes

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ let Trip=document.querySelector(".Trip-Booker")
 console.log("LINKED")
 document.querySelector("button").onclick=function CHECKER(){
     console.log("BUTTON LINK WORKS")
-    if (input.value==="LV101","TEST101") {
+    if (input.value==="LV101","TEST101","BTEST","GCTEST") {
         alert("You have accses and will be redricted to the home page now!Press OK to countinue")
         location.href = 'ACCSES PAGE/index (2).html'
     }
